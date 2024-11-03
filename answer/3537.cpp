9de6c058886b68c0619501a49e993f97f39d39b4
karkit <141783969+karkit7117@users.noreply.github.com>
2024-11-03 13:25:18 +0330

    Create 3537.cpp

---
+++ answer/3537.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x=0;
+    cin>>x;
+    cout<<"W";
+    for (int y=1 ; y<=x ; y++)
+        cout<<"o";
+    cout<<"w!";
+    return 0;
+}
