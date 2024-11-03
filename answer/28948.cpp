5333a11f8133c347c0fd4e35f1797f82a26ad129
karkit <141783969+karkit7117@users.noreply.github.com>
2024-11-03 13:36:15 +0330

    Create 28948.cpp

---
+++ answer/28948.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    if(a>0)
+        cout<<"swqr";
+    if(a>1)
+        cout<<endl<<"synj";
+    if(a>2)
+        cout<<endl<<"seth";
+    if(a>3)
+        cout<<endl<<"sgtrhy";
+    if(a>4)
+        cout<<endl<<"strsh";
+    if(a>5)
+        cout<<endl<<"srgty";
+    if(a>6)
+        cout<<endl<<"srh";
+	return 0;
+}
